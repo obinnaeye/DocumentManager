ca554ce9e549f9da435783da5ec7016c42514356
Nnenanya Obinna <obinna.nnenanya.com>
2017-05-27 08:41:50 +0100

    feature(database):setup postgres database

---
+++ server/migrations/20170527072920-create-user.js
@@ -0,0 +1,58 @@
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      roleId: {
+        type: Sequelize.INTEGER,
+        default: 2,
+        allowNull: false,
+        references: {
+          model: 'Roles',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'no action'
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      activeToken: {
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down(queryInterface) {
+    return queryInterface.dropTable('Users');
+  }
+};

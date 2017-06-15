9db70c6c858df69a6b3328927491fef58eceb3d3
Nnenanya Obinna <obinna.nnenanya.com>
2017-06-15 12:50:21 +0100

    feature(authentication):fix page access authetication

--- client/src/js/components/user/UserPage.js
+++ client/src/js/components/user/UserPage.js
@@ -7,17 +7,18 @@ import DocumentView from '../document/DocumentView'; // remove
 import EditDocument from '../document/EditDocument';
 import SearchPage from './SearchPage';
 import EditProfile from './EditProfile';
+import PageAccessHelper from '../../helper/PageAccessHelper';
 
 const UserPage = () =>
   (
     <div>
-      <Route path="/dashboard" component={SideNav} />
-      <Route exact path="/dashboard/:new-document" component={NewDocument} />
-      <Route exact path="/dashboard/my-documents" component={UserDocuments} />
-      <Route exact path="/dashboard/documents/:id" component={DocumentView} />
-      <Route exact path="/dashboard/edit-document/:id" component={EditDocument} />
-      <Route exact path="/dashboard/search" component={SearchPage} />
-      <Route exact path="/dashboard/edit-profile" component={EditProfile} />
+      <Route path="/dashboard" component={PageAccessHelper(SideNav)} />
+      <Route exact path="/dashboard/:new-document" component={PageAccessHelper(NewDocument)} />
+      <Route exact path="/dashboard/my-documents" component={PageAccessHelper(UserDocuments)} />
+      <Route exact path="/dashboard/documents/:id" component={PageAccessHelper(DocumentView)} />
+      <Route exact path="/dashboard/edit-document/:id" component={PageAccessHelper(EditDocument)} />
+      <Route exact path="/dashboard/search" component={PageAccessHelper(SearchPage)} />
+      <Route exact path="/dashboard/edit-profile" component={PageAccessHelper(EditProfile)} />
     </div>
   );
 
--- client/src/js/helper/PageAccessHelper.js
+++ client/src/js/helper/PageAccessHelper.js
@@ -1,42 +1,57 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as UserActions from '../actions/UserActions';
 
-export default (TargetComponent) => {
-  class PageAccessHelper extends React.Component {
+
+
+export default (ComposedConmponent) => {
+  class Authenticate extends React.Component {
 
     componentWillMount() {
-      console.log(this.props.user);
-    }
-
-    componentWillUpdate(nextProps) {
-      if (!nextProps.user) {
-        this.context.router.push('/signin');
+      if (!this.props.authenticated && !this.props.signingIn) {
+        const { userId } = jwt_decode(localStorage.xsrf_token);
+        this.props.UserActions.validateUser(userId)
+          .then(() => {
+            console.log(this.props.authenticated);
+          })
+          .catch((error) => {
+            this.context.router.history.push('/');
+          });
       }
     }
 
-    /**
-     * render
-     * @method
-     * @returns {jsx} - jsx
-     */
+    componentWillReceiveProps(nextProps) {
+      if (!nextProps.authenticated && !nextProps.signingIn) {
+        this.context.router.history.push('/');
+      }
+    }
+
     render() {
       return (
-        <TargetComponent />
+        <ComposedConmponent />
       );
     }
   }
 
-  PageAccessHelper.propTypes = {
-    user: PropTypes.object.isRequired
+  Authenticate.propTypes = {
+    authenticated: PropTypes.bool.isRequired,
+    signingIn: PropTypes.bool.isRequired
   };
 
-  // PageAccessHelper.contextTypes = {
-  //   router: PropTypes.object.isRequired
-  // };
+  Authenticate.contextTypes = {
+    router: PropTypes.object.isRequired
+  };
 
   const mapStateToProps = state => ({
-    user: state
+    authenticated: state.userReducers.authenticated,
+    signingIn: state.signInReducer.signingIn
   });
-  return connect(mapStateToProps)(PageAccessHelper);
+
+  const mapDispatchToProps = dispatch => ({
+    UserActions: bindActionCreators(UserActions, dispatch)
+  });
+
+  return connect(mapStateToProps, mapDispatchToProps)(Authenticate);
 };
 
